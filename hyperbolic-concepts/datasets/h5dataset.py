626fe95d90c3d89a79919b4b347c1d320560fa56
Michael Goodale <michael.goodale@mail.mcgill.ca>
2021-12-02 20:57:29 +0100

    Added dataset maker

--- hyperbolic-concepts/datasets/h5dataset.py
+++ hyperbolic-concepts/datasets/h5dataset.py
@@ -1,6 +1,12 @@
 import torch
 import h5py
 
+from torchvision import datasets, transforms
+import numpy as np
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+import h5py
 
 class H5Dataset(torch.utils.data.Dataset):
 
@@ -27,3 +33,29 @@ class H5Dataset(torch.utils.data.Dataset):
 
   def __len__(self):
       return self.length
+
+
+def create_dataset(data_path: str, h5file_path: str):
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.PILToTensor()
+    ])
+
+    animal_data = datasets.ImageFolder(data_path, transform=preprocess)
+    channel, width, height = [3, 224, 224]
+    N_IMAGES = (len(animal_data))
+    batch_size = 512
+
+    #TODO: Split into val, train and test.
+
+    loader = torch.utils.data.DataLoader(animal_data,
+                                              batch_size=batch_size,
+                                              num_workers=2)
+
+    with h5py.File(h5file_path, 'w') as  h5f:
+      img_ds = h5f.create_dataset('images', shape=(N_IMAGES, channel, width, height), dtype='uint8', chunks=(32, 3, 224, 224))
+      target_ds = h5f.create_dataset('targets', shape=(N_IMAGES), dtype=int, chunks=True)
+      for i, (image, target) in enumerate(tqdm(loader)):
+        img_ds[i*batch_size:(i+1)*batch_size, :, :, :] = image
+        target_ds[i*batch_size:(i+1)*batch_size] = target
