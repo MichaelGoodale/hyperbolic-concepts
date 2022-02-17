d103f47c770dbb6f6a59c0a65f921292d53fa012
Michael Goodale <michael.goodale@mail.mcgill.ca>
2022-02-17 15:43:51 +0100

    fixed typo

--- hyperbolic_concepts/losses/concept_loss.py
+++ hyperbolic_concepts/losses/concept_loss.py
@@ -7,7 +7,7 @@ from typing import Callable
 
 class ConceptClassificationLoss(nn.Module):
 
-    def __init__(self, self.model, margin=0.5, negative_sample_ratio:float = 10.0):
+    def __init__(self, model, margin=0.5, negative_sample_ratio:float = 10.0):
         ''' concepts: a NXD tensor where N is number of concepts and D is dimensionality of the space
             radii: a N tensor where each row is the radius of the corresponding conept in conepts'''
         super(ConceptClassificationLoss, self).__init__()
